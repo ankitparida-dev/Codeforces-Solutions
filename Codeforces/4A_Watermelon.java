2e0666883f2fa735586e26c3e87c0d1db7d92416
Ankit Parida <ankitparida386@gmail.com>
2026-09-19 12:35:49 +0530

    Sync Codeforces: 4A - Watermelon [Java 21]

--- Codeforces/4A_Watermelon.java
+++ Codeforces/4A_Watermelon.java
@@ -2,9 +2,8 @@ import java.util.*;
  
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
- 
-        int w = sc.nextInt();
+        Scanner s = new Scanner(System.in);
+        int w = s.nextInt();
  
         if (w > 2 && w % 2 == 0) {
             System.out.println("YES");
